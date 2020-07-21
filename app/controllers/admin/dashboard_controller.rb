7680fda48c5852a5f92a459210b792c1ebd855ad
alex-hladun <alexhladun@gmail.com>
2020-07-21 21:11:01 +0000

    Implement admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,10 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
 
   def show
+    @data = Hash[
+      "product_count" => Product.count(:name),
+      "category_count" => Category.count(:name)
+  ]
+    puts @data
   end
 end
